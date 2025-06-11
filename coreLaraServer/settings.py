bab01c7f4bfe2e74032c4d3a776c5336815211c6
Caio Rocha <caiomzrocha@gmail.com>
2025-06-11 20:53:17 -0300

    ops: add render properties

--- coreLaraServer/settings.py
+++ coreLaraServer/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': int(os.getenv('DB_PORT', 3306)),
+        'PORT': int(os.getenv('DB_PORT', 5432)),
     }
 }
 
